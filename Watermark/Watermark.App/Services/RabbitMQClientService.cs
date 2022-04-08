25569472ea7767a72e198556a78b4222adbc875f
yapmazenes <yapmazenes@gmail.com>
2022-04-08 18:27:18 +0300

    Watermark App

---
+++ Watermark/Watermark.App/Services/RabbitMQClientService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+
+namespace Watermark.App.Services
+{
+    public class RabbitMQClientService : IDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        public static string ExchangeName = "ImageDirectExchange";
+        public static string RoutingWaterMark = "watermark-route";
+        public static string QueueName = "queue-watermark-image";
+        private readonly ILogger<RabbitMQClientService> _logger;
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+        }
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            if (_channel is { IsOpen: true })
+                return _channel;
+
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(ExchangeName, type: "direct", true, false);
+            _channel.QueueDeclare(QueueName, true, false, false, null);
+            _channel.QueueBind(QueueName, ExchangeName, RoutingWaterMark);
+            _logger.LogInformation("Connected with RabbitMq");
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+            _channel = null;
+            _connection?.Close();
+            _connection?.Dispose();
+            _logger.LogInformation("Disconnected with RabbitMq");
+        }
+    }
+}
